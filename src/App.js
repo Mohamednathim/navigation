5844bf0bd54cc720f16725654ffea657704f0003
Mohamednathim <mohamednathim84@gmail.com>
2023-12-20 01:21:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import All from './components/All';
+import Career from './components/Career';
+import CyberSecurity from './components/CyberSecurity';
+import DataScience from './components/DataScience';
+import FullStackDevelopment from './components/FullStackDevelopment';
+import { Link, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <>
+            <nav className='navbar d-flex justify-content-center align-items-center navbar-expand-lg navbar-light bg-light'>
+                <ul className='navbar-nav'>
+                    <li className='nav-item'><Link className='nav-link' to='/'>All</Link></li>
+                    <li className='nav-item'><Link className='nav-link' to='/FullStackDevelopment'>FULL STACK DEVOLOPMENT</Link></li>
+                    <li className='nav-item'><Link className='nav-link' to='/DataScience'>DATA SCIENCE</Link></li>
+                    <li className='nav-item'><Link className='nav-link' to='/CyberSecurity'>CYBER SECURITY</Link></li>
+                    <li className='nav-item'><Link className='nav-link' to='/Career'>CAREER</Link></li>
+                </ul>
+            </nav>
+
+
+
+            <Routes>
+                <Route path='/' element={<All />} />
+                <Route path='/FullStackDevelopment' element={<FullStackDevelopment />} />
+                <Route path='/DataScience' element={<DataScience />} />
+                <Route path='/CyberSecurity' element={<CyberSecurity />} />
+                <Route path='/Career' element={<Career />} />
+            </Routes>
+        </>
+
+    );
 }
+
 
 export default App;
